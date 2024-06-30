23459c7cd7b47293feb9d849c3417fec21244eea
TFSID <tegar.firman215@gmail.com>
2024-06-30 22:07:26 +0700

    add featured store

--- src/app/event-content.tsx
+++ src/app/event-content.tsx
@@ -11,27 +11,29 @@ import EventContentCard from "@/components/event-content-card";
 
 const EVENT_CONTENT = [
   {
-    title: "AI's Role in Shaping the Future",
-    des: "Discover how AI is revolutionizing industries, creating new possibilities, and addressing global challenges. Gain a deep understanding of the pivotal role AI plays in driving innovation, sustainability, and progress in our rapidly evolving world.",
-    name: "Marcell Glock",
-    position: "Chief Executive, Spotify",
-    panel: "Panel Discussion",
-    img: "/image/avatar1.jpg",
-  },
+    title: "@cilokrentcos",
+    des: "RENT KOSTUM & STYLING-REPAIR WIG",
+    name: "@cilokrentcos",
+    link: "https://www.instagram.com/cilokrentcos?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    model_toko: "Rentcos",
+    img: "/logos/cilokrentcos.webp",
+  }
+];
+const dummy_data = [
   {
     title: "Introduction to Machine Learning",
     des: "Explore the basic principles, algorithms, and applications of Machine Learning. Through hands-on exercises and practical examples, you'll develop a solid understanding of how Machine Learning powers AI-driven solutions.",
     name: "Marcell Glock",
-    position: "Chief Executive, Spotify",
-    panel: "Workshop",
+    link: "Chief Executive, Spotify",
+    model_toko: "Merch",
     img: "/image/avatar2.jpg",
   },
   {
     title: "AI in Healthcare: Revolutionizing Patient Care",
     des: "This session is a must-attend for healthcare professionals, AI enthusiasts, and anyone interested in the intersection of technology and well-being. Join us as we discuss how AI is bringing about positive changes in healthcare.",
     name: "Marcell Glock",
-    position: "Chief Executive, Spotify",
-    panel: "Workshop",
+    link: "Chief Executive, Spotify",
+    model_toko: "Workshop",
     img: "/image/avatar3.jpg",
   },
 ];
@@ -41,22 +43,19 @@ export function EventContent() {
     <section className="py-8 px-8 lg:py-20">
       <Tabs value="Day1" className="mb-8">
         <div className="w-full flex mb-8 flex-col items-center">
-          <TabsHeader className="h-12 w-72 md:w-96">
-            <Tab value="Day1" className="font-medium">
-              Day 1
-            </Tab>
-            <Tab value="Day2" className="font-medium">
-              Day 2
-            </Tab>
-            <Tab value="Day3" className="font-medium">
-              Day 3
+          <TabsHeader placeholder="Tab Header" className="h-12 w-72 md:w-96">
+            <Tab placeholder="Tabs" value="Day1" className="font-medium">
+              AniMae Featured Store / Trusted Store
             </Tab>
           </TabsHeader>
         </div>
       </Tabs>
       <div className="mx-auto container">
         {EVENT_CONTENT.map((props, idx) => (
+          
           <EventContentCard key={idx} {...props} />
+          
+          
         ))}
       </div>
     </section>
--- src/components/event-content-card.tsx
+++ src/components/event-content-card.tsx
@@ -11,61 +11,49 @@ interface EventContentCardProps {
   title: string;
   des: string;
   name: string;
-  position: string;
-  panel: string;
+  link: string;
+  model_toko: string;
   img: string;
 }
 export function EventContentCard({
   title,
   des,
   name,
-  position,
-  panel,
+  link,
+  model_toko,
   img,
 }: EventContentCardProps) {
   return (
-    <Card
-      color="transparent"
-      shadow={false}
-      className="lg:!flex-row mb-10 lg:items-end"
-    >
-      <CardHeader
-        floated={false}
-        shadow={false}
-        className="h-[32rem] max-w-[28rem] shrink-0"
-      >
-        <Image
-          width={768}
-          height={768}
-          src={img}
-          alt="testimonial image"
-          className="h-full w-full object-cover"
-        />
+    <Card placeholder="card" color="transparent" shadow={false} className="lg:!flex-row mb-10 lg:items-center">
+      <CardHeader placeholder="cardHeader" floated={false} shadow={false} className="h-auto max-w-[28rem] shrink-0 justify-content-center">
+        <Image width={240} height={240} src={img} alt="testimonial image" className="rounded-full object-cover object-center"/>
       </CardHeader>
-      <CardBody className="col-span-full lg:col-span-3">
-        <Typography variant="h6" color="blue-gray" className="mb-4">
-          {panel}
+      <CardBody placeholder="cardBody" className="col-span-full lg:col-span-3">
+        <Typography placeholder="model toko" variant="h6" color="blue-gray" className="mb-4">
+          {model_toko}
         </Typography>
-        <Typography variant="h2" color="blue-gray" className="mb-4 font-medium">
+
+        <Typography placeholder="judul" variant="h2" color="blue-gray" className="mb-4 font-medium">
           {title}
         </Typography>
-        <Typography className="mb-12 md:w-8/12 font-medium !text-gray-500">
+
+        <Typography placeholder="deskripsi" className="mb-12 md:w-8/12 font-medium !text-gray-500">
           {des}
         </Typography>
+
         <div className="flex items-center gap-4">
-          <Avatar
-            variant="circular"
-            src="/logos/spotify-logo.png"
-            alt="spotify"
-            size="lg"
-          />
+
+          <Avatar placeholder="logo-instagram" className="object-cover" variant="square" src="/logos/instagram-logo.png" alt="instagram" size="lg" />
           <div>
-            <Typography variant="h6" color="blue-gray" className="mb-0.5">
+
+            <Typography placeholder="nama" variant="h6" color="blue-gray" className="mb-0.5">
               {name}
             </Typography>
-            <Typography variant="small" className="font-normal !text-gray-500">
-              {position}
+
+            <Typography placeholder="link" variant="small" className="font-normal !text-blue-500">
+              <a href={link}>{link}</a>
             </Typography>
+
           </div>
         </div>
       </CardBody>
