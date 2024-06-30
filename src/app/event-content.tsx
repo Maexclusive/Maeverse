7f6d508d30efccc1031e1c51a234b486e58133d4
TFSID <tegar.firman215@gmail.com>
2024-06-30 22:50:13 +0700

    update featured store list

--- src/app/event-content.tsx
+++ src/app/event-content.tsx
@@ -16,24 +16,25 @@ const EVENT_CONTENT = [
     name: "@cilokrentcos",
     link: "https://www.instagram.com/cilokrentcos?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
     model_toko: "Rentcos",
-    img: "/logos/cilokrentcos.webp",
+    img: "/image/cilokrentcos.webp",
+  },
+  {
+    title: "@cumistylistwig",
+    des: "Wig Styling & Repair Comission since 2023. Part Of @cilokrentcos",
+    name: "@cumistylistwig",
+    link: "https://www.instagram.com/cumistylistwig?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    model_toko: "Wig Styling Comission",
+    img: "/image/cumistylistwig.jpg",
   }
 ];
 const dummy_data = [
-  {
-    title: "Introduction to Machine Learning",
-    des: "Explore the basic principles, algorithms, and applications of Machine Learning. Through hands-on exercises and practical examples, you'll develop a solid understanding of how Machine Learning powers AI-driven solutions.",
-    name: "Marcell Glock",
-    link: "Chief Executive, Spotify",
-    model_toko: "Merch",
-    img: "/image/avatar2.jpg",
-  },
+  ,
   {
     title: "AI in Healthcare: Revolutionizing Patient Care",
     des: "This session is a must-attend for healthcare professionals, AI enthusiasts, and anyone interested in the intersection of technology and well-being. Join us as we discuss how AI is bringing about positive changes in healthcare.",
     name: "Marcell Glock",
     link: "Chief Executive, Spotify",
-    model_toko: "Workshop",
+    model_toko: "Merch",
     img: "/image/avatar3.jpg",
   },
 ];
